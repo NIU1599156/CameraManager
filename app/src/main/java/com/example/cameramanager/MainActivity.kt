175f99e4d445a746bc3cb578428be53158b5ae03
NIU1599156 <ivan.penarando@gmail.com>
2024-05-30 12:26:03 +0200

    Added toolbar for application name

--- app/src/main/java/com/example/cameramanager/MainActivity.kt
+++ app/src/main/java/com/example/cameramanager/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.cameramanager
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        recyclerView = findViewById(R.id.recyclerView)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val fabAddCamera: FloatingActionButton = findViewById(R.id.fab_add_camera)
+        fabAddCamera.setOnClickListener {
+            // Procedimientos para añadir cámara
         }
     }
 }
